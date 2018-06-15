cb60a10fd52e2dbf479a9d3daed131918de1dc86
Danil <danil.v.annenkov@gmail.com>
2018-06-15 14:35:39 +0200

    Comments in n-step proof. Cleanup

--- Coq/Payoff/CutPayoffN.v
+++ Coq/Payoff/CutPayoffN.v
@@ -1,4 +1,28 @@
-(* Lemmas and sketch of the proof of the n-step reduction *)
+(** Lemmas and sketch of the proof of the n-step reduction *)
+
+(** ** Overview *)
+
+(** We generalize one-step soundness proof and show by induction on
+the structure of contracts that if cutPayoff() is sound at step n,
+then it is sound at step n+1 (formulation up to omitting some
+technical details):
+[cutPayoff_sound_step : ∀ (c : Contr) (n : nat), cutPayoff_sound c n -> cutPayoff_sound c (1 + n)]
+ The proof is by structural induction on [c] using the "inversion" lemmas
+(e.g. [cp_Scale_inversion : ∀ e c n, cutPayoff_sound (Scale e c) n -> cutPayoff_sound c n])
+allowing to get required premisses for the induction hypotheses.
+We pay special attention to the case of [Tranlate] constructor: we use [cp_summ] to connect the
+output of the evaluation after [n] steps and [n+1] steps.
+
+Then, we can prove the general statement for any [n] :
+[cutPayoff_sound_n_steps : ∀ c n, cutPayoff_sound c n]
+by induction on [n] where base case is just soundness of compilation, and for the
+induction step we use cutPayoff_sound_step.
+
+ Note, that cutPayoff_sound is formulated in more "semantic" style and avoids speaking about
+ n-step reduction directly. Instead we formulate the soundness in terms of traces, and this
+ point of view directly corresponds to the traces produced by the reduction semantics
+ (see lemmas [red_sound1] and [red_sound1] in  ../Reduction.v)*)
+
 Require Import Tactics.
 Require Import ILSyntax.
 Require Import ILSemantics.
@@ -38,44 +62,9 @@ Proof.
   econstructor. econstructor. econstructor. reflexivity.
 Qed.
 
-Lemma adv_disc_0 d:
-  adv_disc 0 d = d.
-Proof.
-  unfold adv_disc. simpl. apply functional_extensionality. reflexivity.
-Qed.
-
-Lemma fromContr_Red_exists c c' t0 tr extp envp il_e:
-  fromContr c t0 = Some il_e ->
-  Red c envp extp c' tr ->
-  exists il_e', fromContr c' t0 = Some il_e'.
-Proof.
-Admitted.
-
-Lemma fromContr_RedN_exists c c' n t0 extp envp il_e:
-  fromContr c t0 = Some il_e ->
-  RedN n c envp extp c' ->
-  exists il_e', fromContr c' t0 = Some il_e'.
-Proof.
-  Admitted.
-
-Lemma fromContr_ILsem_Red_exists c c' t0 tr extp envp il_e il_e' disc p1 p2 extIL tenv r:
-  fromContr c t0 = Some il_e ->
-  Red c envp extp c' tr ->
-  fromContr c' t0 = Some il_e' ->
-  IL[|il_e|]extIL tenv 0 0 disc p1 p2 = Some (ILRVal r) ->
-  exists r', IL[|cutPayoff il_e'|](adv_ext 1 extIL) tenv 0 1 disc p1 p2 = Some (ILRVal r').
-Proof.
-  Admitted.
-
-Lemma fromContr_ILsem_RedN_exists c c' n t0 extp envp il_e il_e' disc p1 p2 extIL tenv r:
-  fromContr c t0 = Some il_e ->
-  RedN n c envp extp c' ->
-  fromContr c' t0 = Some il_e' ->
-  IL[|cutPayoff il_e|]extIL tenv 0 (S n) disc p1 p2 = Some (ILRVal r) ->
-  exists r', IL[|cutPayoff il_e'|](adv_ext (Z.of_nat n) extIL) tenv 0 1 disc p1 p2 = Some (ILRVal r').
-Proof.
-Admitted.
-
+(** Soundness of cutPayoff() fomlulated in terms of traces.
+    Notice that we use generalized formulation that works for arbitrary
+    initial value [n0]. *)
 Definition cutPayoff_sound c n g n0 :=
   forall envC extC (il_e : ILExpr) (extIL : ExtEnv' ILVal) (envP : EnvP) (extP : ExtEnvP)
          (v' : R) m p1 p2 curr v trace (disc : nat -> R ) tenv,
@@ -95,6 +84,7 @@ Definition cutPayoff_sound c n g n0 :=
 
 Hint Resolve delay_empty_trace.
 
+(** Proof of the basic case when [n=0] i.e. we do not ignore any payoffs *)
 Lemma cutPayoff_sound_base c g t0 : cutPayoff_sound c 0 g t0.
 Proof.
   unfold cutPayoff_sound. intros.
@@ -161,6 +151,8 @@ Proof.
     admit.
 Admitted.
 
+
+(** ** Inversion lemmas *)
 Lemma cp_Scale_inversion c n e il G t0:
   G |-E e ∶ REAL ->
   (fromExp (ILTexprZ (ILTexpr (Tnum t0))) e = Some il) ->
@@ -210,36 +202,21 @@ Proof.
   - replace (S n + t0) with (n + S t0) by omega. assumption.
  Admitted.
 
+(** The important observation for proving the [Translate] case in the
+    n-step soundness theorem is that
 
-Lemma sum_delay_n':
-  forall (n n0 : nat) (tr : Trace) (p1 p2 : Party) (curr : Asset) (f : nat -> R) (t0 n1 : nat),
-  sum_list (map (fun t : nat => (f t * delay_trace (n0 + n) tr t p1 p2 curr)%R) (seq (t0 + n0) n1)) =
-  sum_list (map (fun t : nat => (f (n0 + t)%nat * delay_trace n tr t p1 p2 curr)%R) (seq t0 n1)).
-Proof.
-  Admitted.
+    [IL[|cutPayoff il_e|] n = disc(n) * trace(n) + [IL[|cutPayoff
+    il_e|] (1+n)]
 
-Lemma cp_sound_translate_1 c n g t0:
-  cutPayoff_sound c n g (S t0) -> cutPayoff_sound (Translate (Tnum 1) c) (1 + n) g t0.
-Proof.
-  unfold cutPayoff_sound.
-  intros until tenv; intros A Xeq TyExt TyEnv TyC Hclosed TC Cs Hor Sum ILs.
-  inversion Hclosed. inversion TyC. subst.
-  eapply H with (m:=m);eauto.
-  + rewrite Equivalence.transl_iter in Cs. replace (S t0) with (t0 + 1) by ring.
-    apply Cs.
-  + simpl in *. destruct (horizon c tenv). destruct m;simpl in *;tryfalse.
-    simpl in Hor. omega.
-  + simpl in *. replace (n + S t0) with (S (n + t0)) by ring. reflexivity.
-  + replace (n + S t0) with (S (n + t0)) by ring. eapply ILs.
-Qed.
+    That is, cutting payoffs before [n] is exactly a discounted value
+    of a contract trace at the point [n] plus the value we get by
+    cutting payoffs before [n+1]. Intuitively, [disc(n) * trace(n)] is
+    the value that we throw away when we move "current time" from [n]
+    to [n+1]
 
-Fixpoint transl_one_iter (n : nat) (c : Contr) :=
-  match n with
-  | O => Translate (Tnum 1) c
-  | S n' => Translate (Tnum 1) (transl_one_iter n' c)
-  end.
-
-(* I think, this property is required for the n-step proof *)
+    As usual in the statement of the lemma we generalize expressions
+    to work for any "initial time" [n0]. This is a usual approach when
+    proving lemmas about function with the accumulator. *)
 Lemma cp_summ c n :
   forall envC extC (il_e : ILExpr) (extIL : ExtEnv' ILVal) (envP : EnvP) (extP : ExtEnvP)
          (v' : R) m n0 p1 p2 curr v v' trace (disc : nat -> R ) tenv,
@@ -262,24 +239,49 @@ Proof.
   intros H. apply f_equal. inversion H. eapply Rplus_eq_reg_l;eauto.
 Qed.
 
+
+(** To simplify the development without limiting the generality, we limit ourselves to the contracts
+    where [Tranlate] is defined only for a constant [1], i.e. it is of the form [Translate (Tnum 1)].
+    Notice, that we do not consider template variables, since contract reduction in our setting is
+    only possible after instantiating all the template variables. *)
+Inductive Contr_unit_translate : Contr -> Set :=
+| contr_ut_zero : Contr_unit_translate Zero
+| contr_ut_let c : forall e, Contr_unit_translate c -> Contr_unit_translate (Let e c)
+| contr_ut_transfer cur p1 p2 : Contr_unit_translate (Transfer cur p1 p2)
+| contr_ut_scale e c : Contr_unit_translate c -> Contr_unit_translate (Scale e c)
+| contr_ut_translate c : Contr_unit_translate c -> Contr_unit_translate (Translate (Tnum 1) c)
+| contr_ut_both c1 c2 : Contr_unit_translate c1 -> Contr_unit_translate c2 -> Contr_unit_translate (Both c1 c2)
+| contr_ut_if n e c1 c2: Contr_unit_translate c1 -> Contr_unit_translate c2 -> Contr_unit_translate (If e (Tnum n) c1 c2).
+
+(** We define this as a "syntactic sugar" replacement for [Translate (Tnum n)] *)
+Fixpoint transl_one_iter (n : nat) (c : Contr) :=
+  match n with
+  | O => Translate (Tnum 1) c
+  | S n' => Translate (Tnum 1) (transl_one_iter n' c)
+  end.
+
+Example contr_ut_1 : Contr_unit_translate (transl_one_iter 5 Zero).
+Proof. repeat constructor. Qed.
+
 Lemma cutPayoff_sound_step c n g :
-      cutPayoff_sound c n g 0 -> cutPayoff_sound c (1 + n) g 0.
+  Contr_unit_translate c ->
+  cutPayoff_sound c n g 0 -> cutPayoff_sound c (1 + n) g 0.
 Proof.
-  generalize dependent n.
-  induction c;intros until tenv; intros A Xeq TyExt TyEnv TyC Hclosed TC Cs Hor Sum ILs; inversion Hclosed; inversion TyC.
-  - (* zero *)
+  generalize dependent n.  generalize dependent g.
+  induction c;intros G n Hctr_ut H; intros until tenv; intros A Xeq TyExt TyEnv TyC Hclosed TC Cs Hor Sum ILs; inversion Hclosed; inversion TyC; inversion Hctr_ut;subst.
+  - (* Zero *)
     simpl in *. some_inv. subst. simpl in *.
     unfold compose,bind,pure in *. some_inv. rewrite sum_of_map_empty_trace; auto.
-  - (* let *)
+  - (* Let *)
     simpl in *. option_inv_auto.
-  - (* transf *)
+  - (* Transfer *)
     intros. simpl in *. option_inv_auto. unfold compose in *. some_inv.
     remember (Party.eqb p p0) as b0.
     destruct b0.
     + simpl in *. assert (Hmeq0 : m=0) by omega. subst. simpl. some_inv. reflexivity.
     + simpl in *. assert (Hmeq0 : m=0) by omega. assert (Hneq0 : n = 0 ) by omega. subst. simpl.
       rewrite plus_0_l in *.  some_inv. reflexivity.
-  - (* scale *)
+  - (* Scale *)
     intros. simpl in *. option_inv_auto. some_inv. subst. simpl in *. option_inv_auto.
     destruct_vals. some_inv. subst. rewrite delay_trace_scale.
     unfold scale_trace, compose, scale_trans.
@@ -290,53 +292,43 @@ Proof.
     + eapply cp_Scale_inversion in H;eauto.
       eapply IHc with (envC:=envC); eauto.
       simpl. autounfold.
-      rewrite H2. reflexivity.
-  - (* translate *)
-    intros.
+      rewrite H3. reflexivity.
+  - (* Translate *)
+    intros. inversion H7. subst. simpl in *.
     simpl in *. option_inv_auto. rewrite delay_trace_swap.
-    inversion TC. subst. simpl in *.
-    simpl in *.
-    rewrite adv_ext_iter in H2.
-    rewrite delay_trace_iter. rewrite add_0_l in *. rewrite add_0_r in *.
-    destruct n0.
-    * simpl in *.
-      apply cp_Translate_inversion_0 in H.
-      eapply IHc with (envC:=envC);eauto.
-      simpl. autounfold. rewrite H2. reflexivity. simpl in *.
-      destruct (horizon c tenv);simpl in *;tryfalse. apply Hor.
-      rewrite add_0_r in *. reflexivity. rewrite add_0_r in *. auto.
-    * unfold cutPayoff_sound in H.
+      unfold cutPayoff_sound in H.
       remember (sum_list
-       (map (fun t1 : nat => (disc t1 * delay_trace (S n0) x0 t1 p1 p2 curr)%R)
+       (map (fun t1 : nat => (disc t1 * delay_trace 1 x0 t1 p1 p2 curr)%R)
             (seq (S n) m))) as q.
-      rewrite add_0_r in *.
-      apply ILRVal_plus with (l:= (disc n * delay_trace (S n0) x0 n p1 p2 curr)%R).
-      rewrite <- fromVal_RVal_eq.
+      rewrite add_0_r in *. rewrite delay_trace_iter. simpl.
+      apply ILRVal_plus with (l:= (disc n * delay_trace 1 x0 n p1 p2 curr)%R).
       eapply H with (tenv:=tenv) (disc:=disc) (m:=1+m);eauto.
-      ** simpl. rewrite add_0_r in *. eauto.
+      (* ** simpl. rewrite add_0_r in *. eauto. *)
       ** simpl. unfold liftM,liftM2,compose,pure,bind.
-         rewrite Zpos_P_of_succ_nat in *. rewrite <- Nat2Z.inj_succ.
-         rewrite adv_ext_iter.
-         replace (0 + Z.of_nat (S n0))%Z with (Z.of_nat (S n0)) in * by ring.
-         rewrite H2. reflexivity.
-      ** admit.
+         rewrite H3. reflexivity.
+      ** simpl. destruct (horizon c tenv);tryfalse. omega.
       ** rewrite delay_trace_iter. rewrite add_0_r in *.
          simpl in *. subst q. reflexivity.
-      ** eapply cp_summ with (n0 :=1+n0);eauto.
-         unfold plus0 in *. destruct (horizon c tenv);tryfalse;eauto. admit.
+      ** eapply cp_summ with (n0 :=1) (m:=m);eauto.
+         unfold plus0 in *. destruct (horizon c tenv);tryfalse;eauto.
+         omega.
   - (* Both*)
     (* We split values on two summands an apply induction hypothes
        Here we use an inversion principle for [both] (similar to [scale]) to
        obtain the premises for IHs *)
     admit.
-  - (* If-within *)
-    (* We proceed by case analisys on the outcome of evaluation of the contition.
-       We perform inner induction on number of iteration for if-within construct *)
+  - (* If (-within) *)
+    (* We proceed by case analisys on the outcome of the contition evaluation.
+       We perform an inner induction on number of iteration for if-within construct *)
+    admit.
 Admitted.
 
-Theorem cutPayoff_sound_n_steps c n g : cutPayoff_sound c n g 0.
+Theorem cutPayoff_sound_n_steps c n g :
+  Contr_unit_translate c -> cutPayoff_sound c n g 0.
 Proof.
-  induction n.
+  induction n;intros H.
   - apply cutPayoff_sound_base.
-  - apply cutPayoff_sound_step. apply IHn.
+  - apply cutPayoff_sound_step.
+    * apply H.
+    * apply IHn;auto.
 Qed.
